c1df4a7f330a3d6d01b305dfaeb489b4313ad850
Charles Edwill Gabut <kengabut@gmail.com>
2026-06-28 19:26:49 -0700

    added contact section

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,9 +4,17 @@ import Image from "next/image";
 import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/dist/client/components/navigation";
+import { useRef } from 'react'
 
 function Home() {
   const [animation, setAnimation] = React.useState(true);
+
+  //======= Scroll Down functionality=======
+  const contactRef = useRef(null);
+  
+  const scrollToContact = () => {
+    contactRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }
 
   React.useEffect(() => {
     // Stop the animation after 3 seconds (3000ms)
@@ -23,6 +31,7 @@ function Home() {
     <div>
       <nav className="frame">
             <Link href="/about"><button>About</button></Link>
+            <button onClick={scrollToContact}>Contact</button>
             <Link href="/projects"><button>Projects</button></Link>
       </nav>
      <img src={animation ? "/gif_folder/MYNAME.gif" : "/gif_folder/MYNAME.png"} 
@@ -34,6 +43,21 @@ function Home() {
        height={300}
        className="profile-picture"
      />
+     
+     <section ref={contactRef} style={{ minHeight: '100vh' }}>
+      <div className="flex justify-center items-center flex-col">
+          <h1>Let's Connect!</h1>
+          <h1>Contact information</h1>
+          <p>Email: kengabut@gmail.com</p>
+          <p>Phone: 657-226-7547</p>
+          <p>Location: Anaheim, CA</p>
+
+          <h1>Connect With Me!</h1>
+      </div> 
+      
+
+
+     </section>
 
     </div>
   )
